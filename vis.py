dfbfc169dc633e2f3769535491da8571395bb595
lli108 <liang.a.li@intel.com>
2020-05-09 16:45:09 +0800

    add code

--- vis.py
+++ vis.py
@@ -1,7 +1,14 @@
 import os
- 
+
+trace_files = []
+
 path = './trace'
 file_list = os.listdir(path)
+for file in file_list:
+    print(file.find('thd-'))
+    if file.find('thd-') != -1:
+        trace_files.append(file)
+
 
 print('done')
 
